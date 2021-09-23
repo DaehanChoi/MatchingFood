c55bf52ca42a2de58e7f69bc276ce31386fec01f
DaehanChoi <zeuse21@naver.com>
2021-09-23 23:30:37 +0900

    first -> sign in layout

--- app/src/main/java/com/example/machingapp/authorization/SignInActivity.kt
+++ app/src/main/java/com/example/machingapp/authorization/SignInActivity.kt
@@ -3,10 +3,17 @@ package com.example.machingapp.authorization
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.machingapp.R
+import com.example.machingapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.signInBtn.setOnClickListener {
+            val email = binding.signInId.text.toString()
+            val pwd = binding.signInPwd.text.toString()
+        }
     }
 }
